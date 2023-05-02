0aae98e56339548631b9be66fe91e3d811a4f28f
Jay2182 <jaykanjia11@gmail.com>
2023-05-02 19:50:59 +0530

    first card done

--- src/components/GrowthJourney.jsx
+++ src/components/GrowthJourney.jsx
@@ -1,99 +1,197 @@
-import React from 'react';
+import React from "react";
 import img1 from "../img/Group.png";
+import "../style.css";
 
 function GrowthJourney() {
-    return (
-        <div className='flex flex-col px-20 bg-black'>
-            <div className='flex h-64 w-full flex-row  justify-between'>
+	return (
+		<div className="flex flex-col px-20 gap-[400px] py-12 bg-black">
+			<div className="flex w-full flex-row justify-between items-center">
+				<div className="w-[50%] grid grid-flow-col grid-cols-[64.14px_auto] gap-6">
+					<div className="w-[64.14px] h-[64.14px]">
+						<div className="box-border rounded-[50%] flex items-center justify-center text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] p-[1px]">
+							<span className="box-border w-[62px] h-[62px] flex justify-center items-center bg-black rounded-[50%] text-white">
+								1
+							</span>
+						</div>
+					</div>
 
-                <p className='rounded-[50%] w-[64.14px] h-[64.14px]   text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D]  p-1 '><span className='h-full w-full bg-black block rounded-full text-white'>1</span></p>
+					<div className="flex flex-col gap-6 items-start">
+						<p className="font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] ">
+							Method Management
+						</p>
+						<p className="text-justify capitalize text-white leading-8">
+							At PruthaTek, we have a standardized and reliable
+							methodology for efficient project management. We
+							combine our technical expertise with proven project
+							management methods to deliver exceptional results
+							and build long-term relationships with our clients.
+						</p>
+						<button className="mt-6 text-white flex gap-8 justify-center items-center border-[1px] border-white px-[38px] py-2 rounded-md">
+							<span>Explore</span>
+							<span>
+								<svg
+									width="18"
+									height="15"
+									viewBox="0 0 18 15"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z"
+										fill="white"
+									/>
+								</svg>
+							</span>
+						</button>
+					</div>
+				</div>
+				<div className="max-w-[45%]">
+					<img src={img1} alt="." />
+				</div>
+			</div>
 
-                <div className='max-w-[45%] flex flex-col gap-4 items-start '>
+			<div className="flex w-full flex-row justify-between items-center">
+				<div className="w-[50%] grid grid-flow-col grid-cols-[64.14px_auto] gap-6">
+					<div className="w-[64.14px] h-[64.14px]">
+						<div className="box-border rounded-[50%] flex items-center justify-center text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] p-[1px]">
+							<span className="box-border w-[62px] h-[62px] flex justify-center items-center bg-black rounded-[50%] text-white">
+								1
+							</span>
+						</div>
+					</div>
 
-                    <p className='font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] '>Method Management</p>
+					<div className="flex flex-col gap-6 items-start">
+						<p className="font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] ">
+							Method Management
+						</p>
+						<p className="text-justify capitalize text-white leading-8">
+							At PruthaTek, we have a standardized and reliable
+							methodology for efficient project management. We
+							combine our technical expertise with proven project
+							management methods to deliver exceptional results
+							and build long-term relationships with our clients.
+						</p>
+						<button className="mt-6 text-white flex gap-8 justify-center items-center border-[1px] border-white px-[38px] py-2 rounded-md">
+							<span>Explore</span>
+							<span>
+								<svg
+									width="18"
+									height="15"
+									viewBox="0 0 18 15"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z"
+										fill="white"
+									/>
+								</svg>
+							</span>
+						</button>
+					</div>
+				</div>
+				<div className="max-w-[45%]">
+					<img src={img1} alt="." />
+				</div>
+			</div>
+			<div className="flex w-full flex-row justify-between items-center">
+				<div className="w-[50%] grid grid-flow-col grid-cols-[64.14px_auto] gap-6">
+					<div className="w-[64.14px] h-[64.14px]">
+						<div className="box-border rounded-[50%] flex items-center justify-center text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] p-[1px]">
+							<span className="box-border w-[62px] h-[62px] flex justify-center items-center bg-black rounded-[50%] text-white">
+								1
+							</span>
+						</div>
+					</div>
 
-                    <p className='text-justify capitalize text-white '>At PruthaTek, we have a standardized and reliable methodology for efficient project management. We combine our technical expertise with proven project management methods to deliver exceptional results and build long-term relationships with our clients.</p>
+					<div className="flex flex-col gap-6 items-start">
+						<p className="font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] ">
+							Method Management
+						</p>
+						<p className="text-justify capitalize text-white leading-8">
+							At PruthaTek, we have a standardized and reliable
+							methodology for efficient project management. We
+							combine our technical expertise with proven project
+							management methods to deliver exceptional results
+							and build long-term relationships with our clients.
+						</p>
+						<button className="mt-6 text-white flex gap-8 justify-center items-center border-[1px] border-white px-[38px] py-2 rounded-md">
+							<span>Explore</span>
+							<span>
+								<svg
+									width="18"
+									height="15"
+									viewBox="0 0 18 15"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z"
+										fill="white"
+									/>
+								</svg>
+							</span>
+						</button>
+					</div>
+				</div>
+				<div className="max-w-[45%]">
+					<img src={img1} alt="." />
+				</div>
+			</div>
+			<div className="flex w-full flex-row justify-between items-center">
+				<div className="w-[50%] grid grid-flow-col grid-cols-[64.14px_auto] gap-6">
+					<div className="w-[64.14px] h-[64.14px]">
+						<div className="box-border rounded-[50%] flex items-center justify-center text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] p-[1px]">
+							<span className="box-border w-[62px] h-[62px] flex justify-center items-center bg-black rounded-[50%] text-white">
+								1
+							</span>
+						</div>
+					</div>
 
-                    <button className="text-white flex gap-3 "><span>Explore</span><span><svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z" fill="white" />
-                    </svg>
-                    </span></button>
-
-                </div>
-                <div className='h-10 bg-green-500 max-w-[45%]'>
-                    <img src={img1} alt="." />
-                </div>
-            </div>
-
-
-
-
-            <div className='flex h-64 w-full flex-row  justify-between'>
-
-                <p className='rounded-[50%] w-[64.14px] h-[64.14px]   text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D]  p-1 '><span className='h-full w-full bg-black block rounded-full text-white'>1</span></p>
-
-                <div className='max-w-[45%] flex flex-col gap-4 items-start '>
-
-                    <p className='font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] '>Method Management</p>
-
-                    <p className='text-justify capitalize text-white '>At PruthaTek, we have a standardized and reliable methodology for efficient project management. We combine our technical expertise with proven project management methods to deliver exceptional results and build long-term relationships with our clients.</p>
-
-                    <button className="text-white flex gap-3 "><span>Explore</span><span><svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z" fill="white" />
-                    </svg>
-                    </span></button>
-
-                </div>
-                <div className='h-10 bg-green-500 max-w-[45%]'>
-                    <img src={img1} alt="." />
-                </div>
-            </div>
-
-            <div className='flex h-64 w-full flex-row  justify-between'>
-
-                <p className='rounded-[50%] w-[64.14px] h-[64.14px]   text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D]  p-1 '><span className='h-full w-full bg-black block rounded-full text-white'>1</span></p>
-
-                <div className='max-w-[45%] flex flex-col gap-4 items-start '>
-
-                    <p className='font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] '>Method Management</p>
-
-                    <p className='text-justify capitalize text-white '>At PruthaTek, we have a standardized and reliable methodology for efficient project management. We combine our technical expertise with proven project management methods to deliver exceptional results and build long-term relationships with our clients.</p>
-
-                    <button className="text-white flex gap-3 "><span>Explore</span><span><svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z" fill="white" />
-                    </svg>
-                    </span></button>
-
-                </div>
-                <div className='h-10 bg-green-500 max-w-[45%]'>
-                    <img src={img1} alt="." />
-                </div>
-            </div>
-
-            <div className='flex h-64 w-full flex-row  justify-between'>
-
-<p className='rounded-[50%] w-[64.14px] h-[64.14px]   text-center font-medium text-[35px] bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D]  p-1 '><span className='h-full w-full bg-black block rounded-full text-white'>4</span></p>
-
-<div className='max-w-[45%] flex flex-col gap-4 items-start '>
-
-    <p className='font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] '>Engagemnet Models</p>
-
-    <p className='text-justify capitalize text-white '>At PruthaTek, we have a standardized and reliable methodology for efficient project management. We combine our technical expertise with proven project management methods to deliver exceptional results and build long-term relationships with our clients.</p>
-
-    <button className="text-white flex gap-3 "><span>Explore</span><span><svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z" fill="white" />
-    </svg>
-    </span></button>
-
-</div>
-<div className='h-10 bg-green-500 max-w-[45%]'>
-    <img src={img1} alt="." />
-</div>
-</div>
-
-        </div>
-
-    )
+					<div className="flex flex-col gap-6 items-start">
+						<p className="font-medium text-[35px] text-transparent bg-clip-text bg-gradient-to-r from-[#FF63F9] via-[#E41EAD] to-[#FF008D] ">
+							Method Management
+						</p>
+						<p className="text-justify capitalize text-white leading-8">
+							At PruthaTek, we have a standardized and reliable
+							methodology for efficient project management. We
+							combine our technical expertise with proven project
+							management methods to deliver exceptional results
+							and build long-term relationships with our clients.
+						</p>
+						<button className="mt-6 text-white flex gap-8 justify-center items-center border-[1px] border-white px-[38px] py-2 rounded-md">
+							<span>Explore</span>
+							<span>
+								<svg
+									width="18"
+									height="15"
+									viewBox="0 0 18 15"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M10.2197 14.0303C10.5126 14.3232 10.9874 14.3232 11.2803 14.0303L17.5303 7.78033C17.8232 7.48744 17.8232 7.01256 17.5303 6.71967L11.2803 0.46967C10.9874 0.176777 10.5126 0.176777 10.2197 0.46967C9.92678 0.762563 9.92678 1.23744 10.2197 1.53033L15.1893 6.5L0.75 6.5C0.335786 6.5 0 6.83579 0 7.25C0 7.66421 0.335786 8 0.75 8L15.1893 8L10.2197 12.9697C9.92678 13.2626 9.92678 13.7374 10.2197 14.0303Z"
+										fill="white"
+									/>
+								</svg>
+							</span>
+						</button>
+					</div>
+				</div>
+				<div className="max-w-[45%]">
+					<img src={img1} alt="." />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default GrowthJourney
+export default GrowthJourney;
